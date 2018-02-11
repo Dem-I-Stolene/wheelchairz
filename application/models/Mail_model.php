7430901a83e5aef51f7159426537593d1276307f
Voss19 <silvermaster13@gmail.com>
2018-02-11 11:35:10 +0100

    added more email thingies

--- application/models/Mail_model.php
+++ application/models/Mail_model.php
@@ -5,15 +5,39 @@ class Mail_model extends CI_Model
 {
 	public function form_mail($userName, $userEmail, $userSubject, $userMessage)
 	{
-		$mailTo = "your@email.com";
+		// Your info
 
-		$this->load->library('email');
+		$yourMail = "your@email.com";
+		$yourName = "Your name";
+
+		$autoResponseSubject = "Your mail was recieved";
+		$autoResponseMessage = "
+									This is an automatic email, please do not respond to it.
+									\n \n
+									Hello {$userName}, we have recieved your message
+									\n
+									Your subject: {$userSubject}
+									\n
+									Your message: {$userMessage}
+								"
+
+		// To your email
 
 		$this->email->from($userEmail, $userName);
-		$this->email->to($mailTo);
+		$this->email->to($yourMail);
 
 		$this->email->subject($userSubject);
 		$this->email->message($userMessage);
+
+		$this->email->send();
+
+		// To the users email
+
+		$this->email->from($yourMail, $yourName);
+		$this->email->to($userEmail);
+
+		$this->email->subject($autoResponseSubject);
+		$this->email->message($autoResponseMessage);
 
 		$this->email->send();
 	}
