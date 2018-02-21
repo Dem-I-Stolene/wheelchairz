ecd4f255f795084dd835d3a0d16dcfc9cdb0accf
Voss19 <silvermaster13@gmail.com>
2018-02-21 14:42:37 +0100

    a

---
+++ application/views/gallery.php
@@ -0,0 +1,3 @@
+<div class="inner">
+	{p_gallery}
+</div>
---
+++ application/views/subsubpage.php
@@ -0,0 +1,5 @@
+<div class="inner">
+	<h1>{p_title}</h1>
+	<span class="image main"><img style="height: 400px; overflow: hidden; width: auto !important; margin: 0 auto;" src="<?php echo base_url('assets/images/{p_image}') ?>" alt="" /></span>
+	<p>{p_content_file}</p>
+</div>
